10df3e4c25c264f665afaa18136cc198533b75e2
Boris Smus <boris@smus.com>
2022-10-25 17:16:09 -0700

    very primitive start

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-import { CausalGraph } from "./causal-graph.js";
+import {CausalGraph} from "./causal-graph.js";
+mermaid.mermaidAPI.initialize({startOnLoad: false});
+
+
+window.addEventListener('DOMContentLoaded', e => {
+
+  const textarea = document.querySelector('textarea');
+  const out = document.querySelector('#mermaid');
+
+  textarea.addEventListener('blur', e => {
+    console.log('Textarea blurred.');
+    const cgml = textarea.value;
+    const graph = new CausalGraph(cgml);
+    const mermaidGraph = graph.toMermaid();
+
+    mermaid.mermaidAPI.render('graphDiv', mermaidGraph, (svgCode) => {
+      out.innerHTML = svgCode;
+    });
+  });
+
+});
